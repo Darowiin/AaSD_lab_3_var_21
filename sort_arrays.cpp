a56fbfbb1322a0f1db14ee98b38decdbb6e59462
Влад Цуненко <vlad.tsunenko.2004@mail.ru>
2023-12-03 23:49:54 +0400

    added extra task(sort from iterators)

--- sort_arrays.cpp
+++ sort_arrays.cpp
@@ -3,7 +3,7 @@
 #include <random>
 #include <vector>
 
-#define SIZE 50000
+#define SIZE 1000
 
 using namespace std;
 
@@ -24,6 +24,21 @@ stats insertion_sort(vector<int>& arr) {
 		}
 	}
 	return result;
+}
+
+template <typename Iterator>
+stats insertion_sort(Iterator begin, Iterator end) {
+    stats result;
+    for (auto i = begin; i != end; ++i) {
+        for (auto j = i; j != begin && *(j - 1) > *j; --j) {
+            ++result.comparison_count;
+
+            iter_swap(j - 1, j);
+
+            result.copy_count++;
+        }
+    }
+    return result;
 }
 
 stats sheker_sort(vector<int>& arr) {
@@ -53,7 +68,36 @@ stats sheker_sort(vector<int>& arr) {
                 ++result.copy_count;
             }
         }
-        left++;
+        ++left;
+    }
+    return result;
+}
+
+template <typename Iterator>
+stats sheker_sort(Iterator begin, Iterator end) {
+    stats result;
+    auto left = begin, right = end - 1;
+    int flag = 1;
+    while ((left < right) && flag > 0) {
+        flag = 0;
+        for (auto i = left; i != right; ++i) {
+            ++result.comparison_count;
+            if (*i > *(i + 1)) {
+                iter_swap(i, i + 1);
+                flag = 1;
+                ++result.copy_count;
+            }
+        }
+        --right;
+        for (auto i = right; i != left; --i) {
+            ++result.comparison_count;
+            if (*(i - 1) > *i) {
+                iter_swap(i - 1, i);
+                flag = 1;
+                ++result.copy_count;
+            }
+        }
+        ++left;
     }
     return result;
 }
@@ -90,6 +134,45 @@ stats heap_sort(vector<int>& arr)
         swap(arr[0], arr[i]);
         ++result.copy_count;
         heapify(arr, i, 0, result);
+    }
+    return result;
+}
+
+template <typename Iterator>
+void heapify(Iterator begin, Iterator end, Iterator it, stats& result) {
+    auto size = distance(begin, end);
+    auto i = distance(begin, it);
+    auto largest = i;
+    auto left = 2 * i + 1;
+    auto right = 2 * i + 2;
+    ++result.comparison_count;
+    if (left < size && *(begin + left) > *(begin + largest)) {
+        largest = left;
+    }
+    ++result.comparison_count;
+    if (right < size && *(begin + right) > *(begin + largest)) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        iter_swap(it, begin + largest);
+        ++result.copy_count;
+        heapify(begin, end, begin + largest, result);
+    }
+}
+
+template <typename Iterator>
+stats heap_sort(Iterator begin, Iterator end) {
+    stats result;
+    auto size = distance(begin, end);
+
+    for (auto i = size / 2 - 1; i >= 0; --i)
+        heapify(begin, end, begin + i, result);
+
+    for (auto i = size - 1; i >= 0; --i) {
+        iter_swap(begin, begin + i);
+        ++result.copy_count;
+        heapify(begin, begin + i, begin, result);
     }
     return result;
 }
@@ -244,4 +327,87 @@ int main() {
         cout << "comparison quantity for inverted vector: " << inverted_stats.comparison_count << endl;
         cout << "copy quantity for inverted vector: " << inverted_stats.copy_count << endl;
     }
+    //Implementation for iterators
+    // Insertion sort for 100 random arrays, 1 sorted, and 1 inverted
+    {
+        std::cout << "Insertion sort(iterators) for 100 random arrays, 1 sorted and 1 inverted" << std::endl;
+        size_t sum_comparison = 0, sum_copy = 0;
+        for (int i = 0; i < 100; ++i) {
+            std::vector<int> arr = generate_random_vector(SIZE);
+            stats tmp = insertion_sort(arr.begin(), arr.end());
+            sum_comparison += tmp.comparison_count;
+            sum_copy += tmp.copy_count;
+        }
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "Average comparison quantity: " << sum_comparison / 100 << std::endl;
+        std::cout << "Average copy quantity: " << sum_copy / 100 << std::endl;
+
+        std::vector<int> sorted = generate_sorted_vector(SIZE);
+        stats sorted_stats = insertion_sort(sorted.begin(), sorted.end());
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "comparison quantity for sorted vector: " << sorted_stats.comparison_count << std::endl;
+        std::cout << "copy quantity for sorted vector: " << sorted_stats.copy_count << std::endl;
+
+        std::vector<int> inverted = generate_inverted_vector(SIZE);
+        stats inverted_stats = insertion_sort(inverted.begin(), inverted.end());
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "comparison quantity for inverted vector: " << inverted_stats.comparison_count << std::endl;
+        std::cout << "copy quantity for inverted vector: " << inverted_stats.copy_count << std::endl;
+    }
+
+    // Sheker sort for 100 random arrays, 1 sorted, and 1 inverted
+    {
+        std::cout << "Sheker sort(iterators) for 100 random arrays, 1 sorted and 1 inverted" << std::endl;
+        size_t sum_comparison = 0, sum_copy = 0;
+        for (int i = 0; i < 100; ++i) {
+            std::vector<int> arr = generate_random_vector(SIZE);
+            stats tmp = sheker_sort(arr.begin(), arr.end());
+            sum_comparison += tmp.comparison_count;
+            sum_copy += tmp.copy_count;
+        }
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "Average comparison quantity: " << sum_comparison / 100 << std::endl;
+        std::cout << "Average copy quantity: " << sum_copy / 100 << std::endl;
+
+        std::vector<int> sorted = generate_sorted_vector(SIZE);
+        stats sorted_stats = sheker_sort(sorted.begin(), sorted.end());
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "comparison quantity for sorted vector: " << sorted_stats.comparison_count << std::endl;
+        std::cout << "copy quantity for sorted vector: " << sorted_stats.copy_count << std::endl;
+
+        std::vector<int> inverted = generate_inverted_vector(SIZE);
+        stats inverted_stats = sheker_sort(inverted.begin(), inverted.end());
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "comparison quantity for inverted vector: " << inverted_stats.comparison_count << std::endl;
+        std::cout << "copy quantity for inverted vector: " << inverted_stats.copy_count << std::endl;
+    }
+
+    // Heap sort for 100 random arrays, 1 sorted, and 1 inverted
+    {
+        std::cout << "Heap sort(iterators) for 100 random arrays, 1 sorted and 1 inverted" << std::endl;
+        size_t sum_comparison = 0, sum_copy = 0;
+        for (int i = 0; i < 100; ++i) {
+            std::vector<int> arr = generate_random_vector(SIZE);
+            stats tmp = heap_sort(arr.begin(), arr.end());
+            sum_comparison += tmp.comparison_count;
+            sum_copy += tmp.copy_count;
+        }
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "Average comparison quantity: " << sum_comparison / 100 << std::endl;
+        std::cout << "Average copy quantity: " << sum_copy / 100 << std::endl;
+
+        std::vector<int> sorted = generate_sorted_vector(SIZE);
+        stats sorted_stats = heap_sort(sorted.begin(), sorted.end());
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "comparison quantity for sorted vector: " << sorted_stats.comparison_count << std::endl;
+        std::cout << "copy quantity for sorted vector: " << sorted_stats.copy_count << std::endl;
+
+        std::vector<int> inverted = generate_inverted_vector(SIZE);
+        stats inverted_stats = heap_sort(inverted.begin(), inverted.end());
+        std::cout << "size: " << SIZE << std::endl;
+        std::cout << "comparison quantity for inverted vector: " << inverted_stats.comparison_count << std::endl;
+        std::cout << "copy quantity for inverted vector: " << inverted_stats.copy_count << std::endl;
+    }
+
+    return 0;
 }
